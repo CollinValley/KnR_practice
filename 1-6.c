ea380e578b96f951a6b9d34a6413131268413c2c
Collin Valley <collin.valley@gmail.com>
2019-01-04 16:52:07 -0800

    Add exercises 1-6 and 1-7

---
+++ 1-6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Copy input to output, one character at a time
+int main() {
+  int c;
+  // modified to show c is always either 0 or 1
+  while(c = (getchar() != EOF)) {
+    printf("C: %d\n", c);
+  }
+  printf("EOFC: %d\n", c);
+}
---
+++ 1-7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+//This program prints the value of EOF
+int main() {
+  printf("Value of EOF: %d\n", EOF);
+}
